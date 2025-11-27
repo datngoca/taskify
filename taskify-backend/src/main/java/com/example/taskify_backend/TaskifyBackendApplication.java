337466a1149291a18f9078156dd6d7adfc33173c
datngoca <datngoc252k3@gmail.com>
2025-11-27 19:23:24 +0700

    feat: use env for backend

--- taskify-backend/src/main/java/com/example/taskify_backend/TaskifyBackendApplication.java
+++ taskify-backend/src/main/java/com/example/taskify_backend/TaskifyBackendApplication.java
@@ -3,10 +3,23 @@ package com.example.taskify_backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+import io.github.cdimascio.dotenv.DotenvEntry;
+
 @SpringBootApplication
 public class TaskifyBackendApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure()
+				.filename(".env")
+				.ignoreIfMissing()
+				.load();
+
+		// đưa tất cả biến .env vào System properties (Spring sẽ đọc được)
+		for (DotenvEntry entry : dotenv.entries()) {
+			System.setProperty(entry.getKey(), entry.getValue());
+		}
+
 		SpringApplication.run(TaskifyBackendApplication.class, args);
 	}
 
