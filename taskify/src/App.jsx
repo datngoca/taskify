ef2616e346f732b3a2474d9165a7256831d55ee0
datngoca <datngoc252k3@gmail.com>
2025-12-02 21:32:31 +0700

    feat: refactor routing to use Outlet for public and private routes, and fix authentication check in RedirectIfLoggedIn

--- taskify/src/App.jsx
+++ taskify/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
 import { publicRoutes, privateRoutes } from "./routes/route";
 import { AuthProvider } from "./context/AuthContext.jsx";
 import PrivateRoute from "./routes/PrivateRoute.jsx";
@@ -8,35 +8,32 @@ function App() {
     <BrowserRouter>
       <AuthProvider>
         <Routes>
-          {publicRoutes.map((route, index) => {
-            const Page = route.component;
-            return (
-              <Route
-                key={index}
-                path={route.path}
-                element={
-                  <RedirectIfLoggedIn>
-                    <Page />
-                  </RedirectIfLoggedIn>
-                }
-              />
-            );
-          })}
+          <Route
+            element={
+              <RedirectIfLoggedIn>
+                <Outlet />
+              </RedirectIfLoggedIn>
+            }
+          >
+            {publicRoutes.map((route, index) => {
+              const Page = route.component;
+              return <Route key={index} path={route.path} element={<Page />} />;
+            })}
+          </Route>
+
           {/* Private routes can be added here similarly using PrivateRoute component */}
-          {privateRoutes.map((route, index) => {
-            const Page = route.component;
-            return (
-              <Route
-                key={index}
-                path={route.path}
-                element={
-                  <PrivateRoute>
-                    <Page />
-                  </PrivateRoute>
-                }
-              />
-            );
-          })}
+          <Route
+            element={
+              <PrivateRoute>
+                <Outlet />
+              </PrivateRoute>
+            }
+          >
+            {privateRoutes.map((route, index) => {
+              const Page = route.component;
+              return <Route key={index} path={route.path} element={<Page />} />;
+            })}
+          </Route>
         </Routes>
       </AuthProvider>
     </BrowserRouter>
--- taskify/src/routes/RedirectIfLoggedIn.jsx
+++ taskify/src/routes/RedirectIfLoggedIn.jsx
@@ -2,6 +2,6 @@ import { Navigate } from "react-router-dom";
 import { useAuth } from "../hooks/useAuth.jsx";
 const RedirectIfLoggedIn = ({ children }) => {
   const { isAuthenticated } = useAuth();
-  return isAuthenticated ? <>{children}</> : <Navigate to="/" replace />;
+  return !isAuthenticated ? <>{children}</> : <Navigate to="/" replace />;
 };
 export default RedirectIfLoggedIn;
