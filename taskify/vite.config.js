b2f00e4cd0883f386d3bf2b87f5427ee14614d8c
datngoca <datngoc252k3@gmail.com>
2025-12-05 13:34:35 +0700

    feat: add jsconfig.json for TypeScript path mapping and configure Vite aliasing

--- taskify/vite.config.js
+++ taskify/vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import path from 'path' // 1. Import thư viện path
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      // 2. Định nghĩa ký tự '@' trỏ về thư mục 'src'
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
